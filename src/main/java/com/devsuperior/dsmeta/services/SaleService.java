03c8d3038a1482344c549535798af9904d23a839
giovanavi <giovana.gvi02@gmail.com>
2025-08-15 23:51:35 -0300

    add método de findReport

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -20,4 +25,37 @@ public class SaleService {
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+	public Page<SaleMinDTO> findReport(String minDateStr, String maxDateStr, String name, Pageable pageable){
+//Se a data final não for informada, considerar a data atual do sistema.
+		LocalDate maxDate;
+		if (maxDateStr == null || maxDateStr.isBlank()) {
+			maxDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		}else{
+			maxDate = LocalDate.parse(maxDateStr);
+		}
+
+//Se a data inicial não for informada, considerar a data de 1 ano antes da data final.
+		LocalDate minDate;
+		if (minDateStr == null || minDateStr.isBlank()){
+			minDate = maxDate.minusYears(1L);
+		}else {
+			minDate = LocalDate.parse(minDateStr);
+		}
+
+//Se o nome não for informado, considerar o texto vazio.
+		String nameFilter;
+		if (name == null || name.isBlank()){
+			nameFilter = "";
+		}else {
+			nameFilter = name;
+		}
+
+		return repository.searchReport(minDate, maxDate, nameFilter, pageable);
+	}
+
+	public Page<SaleMinDTO> findSummary(){
+		return null;
+	}
+
 }
