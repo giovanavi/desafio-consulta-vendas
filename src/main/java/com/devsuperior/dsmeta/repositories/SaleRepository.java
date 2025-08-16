e13176c9db290cb3ddb145142aedfce35e9868ab
giovanavi <giovana.gvi02@gmail.com>
2025-08-16 00:58:39 -0300

    add método de searchSummary

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsmeta.repositories;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleMinReportDTO;
+import com.devsuperior.dsmeta.dto.SaleMinSummaryDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,12 +11,20 @@ import com.devsuperior.dsmeta.entities.Sale;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
-    @Query(value = "SELECT new com.devsuperior.dsmeta.dto.SaleMinDTO(obj.id, obj.amount, obj.date, obj.seller.name) " +
+    @Query(value = "SELECT new com.devsuperior.dsmeta.dto.SaleMinReportDTO(obj.id, obj.amount, obj.date, obj.seller.name) " +
             "FROM Sale obj " +
             "WHERE obj.date BETWEEN :minDate AND :maxDate " +
             "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%',:name,'%'))")
-    Page<SaleMinDTO> searchReport(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
+    Page<SaleMinReportDTO> searchReport(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
+
+    @Query(value = "SELECT new com.devsuperior.dsmeta.dto.SaleMinSummaryDTO(obj.seller.name, SUM(obj.amount)) " +
+            "from Sale obj " +
+            "WHERE obj.date BETWEEN :minDate and :maxDate " +
+            "GROUP BY obj.seller.name ")
+    List<SaleMinSummaryDTO> searchSummary(LocalDate minDate, LocalDate maxDate);
+
 }
